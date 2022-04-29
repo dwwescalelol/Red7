eb2f6dc263110a66571dd00c11279c9997102ea4
Ezzat Abdallah <20049524@stu.mmu.ac.uk>
2022-04-29 22:54:40 +0100

    Got rid of Player interface as not needed, added documentation to player class

--- Red7/IPlayer.cs
+++ Red7/IPlayer.cs
@@ -2,27 +2,14 @@
 {
     internal interface IPlayer
     {
-        /// <summary>
-        /// Takes a card from the deck and adds it to the Hand
-        /// </summary>
-        /// <param name="deck">CardCollection that represents the deck</param>
+
         public void DrawFromDeck(CardCollection deck);
-        /// <summary>
-        /// Removes all the cards from a CardCollection (either Hand or Palette) and adds them to the deck
-        /// </summary>
-        /// <param name="deck">CardCollection that represents the deck</param>
+
         public void ReturnCardsToDeck(CardCollection deck);
 
-        /// <summary>
-        /// Takes a card from the Hand and adds it to the Palette
-        /// </summary>
-        /// <param name="i">int index of card in Hand to play to Palette</param>
+
         public void PlayToPalette(int i);
-        /// <summary>
-        /// Takes a card from the Hand and adds it to the Canvas
-        /// </summary>
-        /// <param name="i">int index of card in Hand to play to Canvas</param>
-        /// <param name="canvas">CardCollection that represents the canvas</param>
+
         public void PlayToCanvas(int i, CardCollection canvas);
     }
 }
--- Red7/Player.cs
+++ Red7/Player.cs
@@ -2,7 +2,7 @@
 
 namespace Red7
 {
-    internal class Player : IPlayer
+    internal class Player
     {
         public string Name { get; set; }
         public CardCollection Hand { get; }
@@ -19,6 +19,10 @@ namespace Red7
             TurnOrder = 0;
         }
 
+        /// <summary>
+        /// Takes a card from the deck and adds it to the Hand
+        /// </summary>
+        /// <param name="deck">CardCollection that represents the deck</param>
         public void DrawFromDeck(CardCollection deck)
         {
             if (deck.GetNumberOfCards() == 0)
@@ -27,6 +31,10 @@ namespace Red7
             Hand.AddCard(deck.RemoveCard(0));
         }
 
+        /// <summary>
+        /// Removes all the cards from a CardCollection (either Hand or Palette) and adds them to the deck
+        /// </summary>
+        /// <param name="deck">CardCollection that represents the deck</param>
         public void ReturnCardsToDeck(CardCollection deck)
         {
             for(int i = 0; i < Palette.GetNumberOfCards(); i++)
@@ -36,6 +44,10 @@ namespace Red7
                 deck.AddCard(Hand.RemoveCard(0));
         }
 
+        /// <summary>
+        /// Takes a card from the Hand and adds it to the Palette
+        /// </summary>
+        /// <param name="i">int index of card in Hand to play to Palette</param>
         public void PlayToPalette(int i)
         {
             if (i >= Hand.GetNumberOfCards())
@@ -47,6 +59,11 @@ namespace Red7
             Palette.AddCard(Hand.RemoveCard(i));
         }
 
+        /// <summary>
+        /// Takes a card from the Hand and adds it to the Canvas
+        /// </summary>
+        /// <param name="i">int index of card in Hand to play to Canvas</param>
+        /// <param name="canvas">CardCollection that represents the canvas</param>
         public void PlayToCanvas(int i, CardCollection canvas)
         {
             if (i >= Hand.GetNumberOfCards())
@@ -55,17 +72,22 @@ namespace Red7
             canvas.AddCard(Hand.RemoveCard(i));
         }
 
+        /// <summary>
+        /// Returns the palette back to the hand, used for undoing moves
+        /// </summary>
         public void ReturnPaletteToHand()
         {
             int lastCardIndex = Palette.GetNumberOfCards() - 1;
             Hand.AddCard(Palette.RemoveCard(lastCardIndex));
         }
 
+        /// <summary>
+        /// Returns the canvas back to the hand, used for undoing moves
+        /// </summary>
         public void ReturnCanvasToHand(CardCollection canvas)
         {
             int lastCardIndex = canvas.GetNumberOfCards()-1;
             Hand.AddCard(canvas.RemoveCard(lastCardIndex));
         }
-
     }
 }
